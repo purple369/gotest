2e411c0f3cd05a1929023e44689d5bc8bd0d3c2f
Ren Shuo <fengyueboqing@126.com>
2025-10-26 19:07:48 +0800

    six

--- main.go
+++ main.go
@@ -13,3 +13,22 @@ func main() {
 func add(a, b int) int {
 	return a + b
 }
+
+// 写一个快速排序算法
+func quickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	pivot := arr[0]
+	var left, right []int
+	for _, v := range arr[1:] {
+		if v < pivot {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+
+	result := append(quickSort(left), pivot)
+	return append(result, quickSort(right)...)
+}
